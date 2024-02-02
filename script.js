8aa25bc4b62b7e004cf15a2db86bd062985bb789
JaapWiebenga <138365481+JaapWiebenga@users.noreply.github.com>
2024-02-02 09:57:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// script.js
+
+function submitForm() {
+    var name = document.getElementById('name').value;
+    var phone = document.getElementById('phone').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    // Hier kun je de logica toevoegen om het formulier te verzenden (bijv. met AJAX).
+    // Voor dit voorbeeld loggen we de ingevoerde gegevens naar de console.
+    console.log("Naam:", name);
+    console.log("Telefoonnummer:", phone);
+    console.log("E-mail:", email);
+    console.log("Bericht:", message);
+}
+
+    
+    
+    
+    
+
+
